11094a57086a48f9d3f87feb1bbb541ca179efc1
Nate-Kleinsorge <110139749+Nate-Kleinsorge@users.noreply.github.com>
2023-02-10 13:32:30 -0500

    fixed bug

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const store = require('../controllers/store');
+const store = require('../db/store');
 
 router.get('/notes', (req, res) => {
     store.getNotes()
