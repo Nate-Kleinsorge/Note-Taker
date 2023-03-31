1a468c010c955ab09e0e2bb8cb19eccb67f8b3a2
Nate-Kleinsorge <110139749+Nate-Kleinsorge@users.noreply.github.com>
2023-03-30 21:30:07 -0400

    updated delete button

--- db/store.js
+++ db/store.js
@@ -40,6 +40,12 @@ class Store{
             .then((updatedNotes) => this.write(updatedNotes))
             .then(() => newNote);
     }
+
+    deleteNote(id) {
+        return this.getNotes()
+            .then((notes) => notes.filter((note) => note.id !== id))
+            .then((filteredNotes) => this.write(filteredNotes));
+    }
 }
 
 module.exports = new Store();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,5 +15,10 @@ router.post('/notes', (req, res) => {
         .catch((err) => res.status(500).json(err));
 });
 
+router.delete('/notes/:id', (req, res) => {
+    store.deleteNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((err) => res.status(500).json(err));
+}) 
 
 module.exports = router;
