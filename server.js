8e664d2aa8599ebaaa21eb2d9645ce4da4f08a0c
Nate-Kleinsorge <110139749+Nate-Kleinsorge@users.noreply.github.com>
2023-03-30 21:46:30 -0400

    fixed bug

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3001 || process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
